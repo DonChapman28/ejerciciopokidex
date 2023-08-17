891bc0b5e49e482a0f304d538f1a0cc994328be5
eSponjosa <e.rojascisterna@outlook.com>
2023-08-17 11:51:27 -0400

    v0.2

--- pokeApi/poke.js
+++ pokeApi/poke.js
@@ -8,22 +8,23 @@ const pokeStats = document.querySelector('[data-stats]');
 
 const typeColors = {
     electric: '#FFEA70',
-    normal: '#B09398',
+    normal: '#f8efe7',
     fire: '#FF675C',
     water: '#0596C7',
-    ice: '#AFEAFD',
+    ice: '#8cfff7',
     rock: '#999799',
-    flying: '#7AE7C7',
+    flying: '#d6e7e7',
     grass: '#4A9681',
     psychic: '#FFC6D9',
-    ghost: '#561D25',
+    ghost: '#68639d',
     bug: '#A2FAA3',
     poison: '#795663',
     ground: '#D2B074',
     dragon: '#DA627D',
     steel: '#1D8A99',
     fighting: '#2F2F2F',
-    default: '#2A1A1F',
+    dark: '#2a2c4d',
+    default: '#2A1A1F'
 };
 
 const buscarPokimoni = event => {
@@ -51,8 +52,8 @@ const renderPokimoniData = data => {
 const setCardColor = types => {
     const colorOne = typeColors[types[0].type.name];
     const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
-    pokeImg.style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
-    pokeImg.style.backgroundSize = ' 5px 5px';
+    pokeImg.style.background = `conic-gradient(${colorTwo} 0deg 45deg, ${colorOne} 0deg 45deg)`;
+    pokeImg.style.backgroundSize = '5px 5px';
     
 }
 
